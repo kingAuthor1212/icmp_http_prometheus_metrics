267bc611f652563e7c07bf1cc3f07cdd2e859e71
kingAuthor1212 <kingauthor1212@gmail.com>
2025-03-31 11:11:12 +0200

    added handler update server

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/go-ping/ping v1.2.0 // indirect
 	github.com/google/uuid v1.2.0 // indirect
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
 	github.com/klauspost/compress v1.17.11 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/prometheus/client_model v0.6.1 // indirect
---
+++ handler.go
@@ -0,0 +1,94 @@
+package main 
+
+
+import(
+	"golang.org/x/net/icmp"
+	"golang.org/x/net/ipv4"
+	"time"
+	"os"
+	"fmt"
+	"net"
+	"net/http"
+)
+/* ping sends an ICMP echo request (ping) to the specified target IP address or hostname.
+It measures the time taken for the request and updates Prometheus metrics accordingly.
+
+Parameters:
+- target: A string representing the target IP address or hostname to ping.
+ICMP packet handling, https://pkg.go.dev/golang.org/x/net/icmp
+package to resolve IP addresses, https://pkg.go.dev/net#ResolveIPAddr
+package for measuring durations, https://pkg.go.dev/time
+*/
+func ping(target string) {
+    c, err := icmp.ListenPacket("ip4:icmp", "0.0.0.0")  
+    if err != nil {
+        fmt.Println("Error listening:", err)
+        return
+    }
+    defer c.Close()
+
+    start := time.Now()
+
+    message := icmp.Message{
+        Type: ipv4.ICMPTypeEcho,
+        Code: 0,
+        Body: &icmp.Echo{
+            ID:   os.Getpid() & 0xffff,
+            Seq:  1,
+            Data: []byte("Ping"),
+        },
+    }
+
+    msgBytes, err := message.Marshal(nil)
+    if err != nil {
+        fmt.Println("Error marshaling message:", err)
+        return
+    }
+
+    addr, err := net.ResolveIPAddr("ip4", target)
+    if err != nil {
+        fmt.Println("Error resolving address:", err)
+        return
+    }
+
+    _, err = c.WriteTo(msgBytes, addr)
+    if err != nil {
+        fmt.Println("Error writing:", err)
+        icmpPingSuccess.WithLabelValues(target).Set(0)
+        return
+    }
+
+    reply := make([]byte, 1024)
+    c.SetDeadline(time.Now().Add(2 * time.Second))
+    _, _, err = c.ReadFrom(reply)
+    duration := time.Since(start).Seconds()
+
+    if err == nil {
+        icmpPingSuccess.WithLabelValues(target).Set(1)
+        icmpPingResponseTime.WithLabelValues(target).Set(duration)
+        fmt.Println("Ping to",target ,"in", duration, "seconds")
+    } else {
+        icmpPingSuccess.WithLabelValues(target).Set(0)
+        fmt.Println("Ping failed", err)
+    }
+}
+
+/* httpGet performs an HTTP GET request to the specified URL.
+It measures the time taken for the request and prints the response time.
+
+Parameters:
+- url: A string representing the target URL for the GET request.
+Go net/http package https://pkg.go.dev/net/http
+*/
+func httpGet(url string) {
+    startTime := time.Now()
+    resp, err := http.Get(url)
+    if err != nil {
+        fmt.Println("HTTP GET request failed:", err)
+        return
+    }
+    defer resp.Body.Close()
+
+    duration := time.Since(startTime).Seconds()
+    fmt.Println("HTTP GET response from",url,"in", duration, "seconds")
+}
